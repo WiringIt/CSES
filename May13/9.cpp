dc410eb4f0a1b33663e5a82c070066fea6abbdc7
WiringIt <coding.up.myself@gmail.com>
2025-05-13 23:20:18 +0530

    Create 9.cpp

---
+++ May13/9.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        a[i] += a[i - 1];
+    }
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << a[r] - a[l - 1] << '\n';
+    }
+}
